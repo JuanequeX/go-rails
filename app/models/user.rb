4ed631402129657712dc1925a9634825c3f17031
Juanequex <ramon.g.uz37@gmail.com>
2023-08-20 00:37:59 -0600

    User Model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# DATA SCHEMA
+#
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
+
+class User < ApplicationRecord
+  has_secure_password
+
+
+end
