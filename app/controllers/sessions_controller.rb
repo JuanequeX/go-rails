f86373800c0d478956741362b7d5388f381684aa
Juanequex <ramon.g.uz37@gmail.com>
2023-08-23 08:58:31 -0600

    sign in form and logout actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+  def new
+    # If the user is already logged in, redirect them to the root page.
+    if session[:user_id]
+      redirect_to root_path
+    end
+  end
+
+  def create
+    # Find the user by their email address
+    user = User.find_by(email: params[:email])
+    # If the user exists AND the password entered is correct.
+    if user.present? && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      # Redirect to the root page with a notice.
+      redirect_to root_path, notice: "Succesfully logged in!"
+    else
+      # If user's login doesn't work, send them back to the login form.
+      flash[:alert] = "Invalid email or password"
+      render "new"
+    end
+  end
+
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Succesfully logged out!"
+  end
+end
