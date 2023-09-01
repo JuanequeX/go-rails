6ccdbe0987c4887c493bcfc1b7e46aee39e166cf
Juanequex <ramon.g.uz37@gmail.com>
2023-08-31 22:38:38 -0600

    Reset pasword mailer

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
 
+  get "password/reset", to: "password_resets#new", as: :new_password_reset
+  post "password/reset", to: "password_resets#create", as: :password_reset
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
