821c17ae25c96a382e0ce715285d09b6070aa60e
Juanequex <ramon.g.uz37@gmail.com>
2023-09-04 01:18:05 -0600

    Twitter Omniauth configuration

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,39 @@
 
 # Path: app/controllers/omniauth_callbacks_controller.rb
 class OmniauthCallbacksController < ApplicationController
+  before_action :require_user_logged_in!
+
   def twitter
-    render plain: 'Success your twitter account is linked!'
+    Rails.logger.info("🔥🔥🔥 #{auth} 🔥🔥🔥")
+    update_or_create_twitter_account
+    redirect_to root_path, notice: 'Successfully connected your account!'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+  def update_or_create_twitter_account
+    twitter_account = find_or_initialize_twitter_account
+
+    update_twitter_account_attributes(twitter_account)
+
+    twitter_account.save!
+  end
+
+  def find_or_initialize_twitter_account
+    Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+  end
+
+  def update_twitter_account_attributes(twitter_account)
+    twitter_account.assign_attributes(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
   end
 end
