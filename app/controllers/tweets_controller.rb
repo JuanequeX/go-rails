b6dddca26e2a2dc2501d90903622172d3bffd610
Juanequex <ramon.g.uz37@gmail.com>
2023-09-10 14:59:27 -0600

    Tweet Validation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,4 +11,20 @@ class TweetsController < ApplicationController
   def new
     @tweet = Current.user.tweets.new
   end
+
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+
+    if @tweet.save
+      redirect_to tweets_path, notice: 'Tweet was scheduled successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
 end
